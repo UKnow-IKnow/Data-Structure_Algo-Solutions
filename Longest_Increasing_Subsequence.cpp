7b678c7906cd5971c8397a4f5bfc6dcff9bc63ad
UKnow-IKnow <sarojsahu014@gmail.com>
2022-08-08 23:27:56 +0530

    Solution of Longest Increasing Subsequence LeetCode(Q.300)

---
+++ Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,19 @@
+//LeetCode Q.300. Longest Increasing Subsequence
+
+//Only logic part
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int ans =1, n = nums.size();
+        vector<int> dp(n,1);
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j >= 0;j--){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i],dp[j]+1);
+                    ans = max(ans,dp[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
