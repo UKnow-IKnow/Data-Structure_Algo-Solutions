24c6a57791885121338661e3785813f3143035b9
UKnow-IKnow <sarojsahu014@gmail.com>
2022-08-26 21:44:48 +0530

    Solution of Reordered Power of 2 LeetCode(Q.869)

---
+++ Reordered_Power_of_2.cpp
@@ -0,0 +1,19 @@
+//LeetCode Q.869. Reordered Power of 2
+
+//only logic part
+
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+        for (int i = 0; i <= 30; i++){
+            int p = pow(2, i);
+            string t = to_string(p);
+            sort(t.begin(), t.end());
+            if (t == s)
+                return true;
+        }
+        return false;
+    }
+};
