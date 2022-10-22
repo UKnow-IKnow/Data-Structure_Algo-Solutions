9caf370e82d983a2db978407da13e4a9b77d53c0
UKnow-IKnow <sarojsahu014@gmail.com>
2022-10-22 22:21:46 +0530

    Solution of Group Anagrams Leetcode(Q.49)

---
+++ LeetCode/Group_Anagrams.cpp
@@ -0,0 +1,20 @@
+//Leetcode Q.49. Group Anagrams
+
+//only logic part
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            string t = s; 
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+        vector<vector<string>> anagrams;
+        for (auto p : mp) { 
+            anagrams.push_back(p.second);
+        }
+        return anagrams;
+    }
+};
