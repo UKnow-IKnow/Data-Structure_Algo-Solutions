c072bb8191372ed18f829d2a653c2a75e26a97c2
UKnow-IKnow <sarojsahu014@gmail.com>
2022-11-04 22:18:29 +0530

    Solution of Minimum Size Subarray Sum Leetcode(Q.209)

---
+++ LeetCode/Minimum_Size_Subarray_Sum.cpp
@@ -0,0 +1,18 @@
+//Leetcode Q.209. Minimum Size Subarray Sum
+
+//only logic part
+
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int l = 0, r = 0, n = nums.size(), sum = 0, len = INT_MAX;
+        while (r < n) {
+            sum += nums[r++];
+            while (sum >= s) {
+                len = min(len, r - l);
+                sum -= nums[l++];
+            }
+        }
+        return len == INT_MAX ? 0 : len;
+    }
+};
