b58958175442123371153f090c437a3402ceb761
UKnow-IKnow <sarojsahu014@gmail.com>
2022-11-08 22:13:11 +0530

    Solution of Make The String Great Leetcode(Q.1544)

---
+++ LeetCode/Make_The_String_Great.cpp
@@ -0,0 +1,23 @@
+//Leetcode Q.1544. Make The String Great
+
+//only logic part
+
+class Solution {
+public:
+    string makeGood(string s) {
+        if(s.size()==1 || s==""){
+           return s;
+        }
+        auto it=s.begin();
+        while(it<s.end() && s.size()>0){
+           if((abs(*it - *(it+1))==32)){
+               s.erase(it,it+2);
+               it=s.begin();
+           }
+           else{
+             it++;
+           }
+        }
+        return s;
+    }
+};
