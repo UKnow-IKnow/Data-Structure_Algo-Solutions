666a009ec2e56f6d97944222d9b2a0038a324b9c
UKnow-IKnow <sarojsahu014@gmail.com>
2022-11-18 21:55:54 +0530

    Solution of  X of a Kind in a Deck of Cards Leetcode(Q.914)

---
+++ LeetCode/X_of_a_Kind_in_a_Deck_of_Cards.cpp
@@ -0,0 +1,33 @@
+//Leetcode Q.914. X of a Kind in a Deck of Cards
+
+//only logic part
+
+class Solution {
+public:
+     int gcd(int a, int b){
+        int tmp;
+        if(a < b){
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+        if(b == 0)return a;
+        return gcd(b, a%b);
+    }
+    
+    bool hasGroupsSizeX(vector<int>& deck) {
+        if(deck.size() < 2) return false;
+        map<int, int> count;
+        for(int e : deck){
+            count[e]++;
+        }
+        
+        int g = 0;
+        for(map<int, int>::iterator it = count.begin(); it != count.end(); it++){
+            g = gcd(g, it->second);
+        }
+        
+        if(g >= 2) return true;
+        return false;
+    }
+};
