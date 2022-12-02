043fb799abe584e93816dbc10a2e1488cd436df0
UKnow-IKnow <sarojsahu014@gmail.com>
2022-12-02 23:19:32 +0530

    Solution of Determine if Two Strings Are Close LeetCode(Q.1657)

---
+++ LeetCode/165Determine_if_Two_Strings_Are_Close.cpp
@@ -0,0 +1,18 @@
+?//leetCode Q.1657. Determine if Two Strings Are Close
+
+//only logic part
+
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        vector<int>m(26,0),m1(26,0);
+        for(auto i:word1)m[i-'a']++;
+        for(auto i:word2)m1[i-'a']++;
+        for(int i=0;i<26;i++){
+            if((m[i]&&!m1[i])||(!m[i]&&m[i]))return false; 
+        }
+        sort(m.begin(),m.end());
+        sort(m1.begin(),m1.end());
+        return m==m1;
+    }
+};
