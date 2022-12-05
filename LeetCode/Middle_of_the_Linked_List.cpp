e7b4cc72c9f508ef2c21229d6a88d34e23178dbd
UKnow-IKnow <sarojsahu014@gmail.com>
2022-12-05 20:24:18 +0530

    Solution of Middle of the Linked List LeetCode(Q.876)

---
+++ LeetCode/Middle_of_the_Linked_List.cpp
@@ -0,0 +1,28 @@
+//LeetCode Q.876. Middle of the Linked List
+
+//only logic part
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* step1 = head;
+        ListNode* step2 = head;
+
+        while(step2!=NULL && step2->next!=NULL){
+            step1 = step1->next;
+            step2 = step2->next->next;
+        }
+        return step1;
+    }
+};
+   
