121ee015d209905eb30ab0baa768d48e858cc891
UKnow-IKnow <sarojsahu014@gmail.com>
2022-12-16 22:35:26 +0530

    Solution of Implement Queue using Stacks Leetcode(Q.232)

---
+++ LeetCode/Implement_Queue_using_Stacks.cpp
@@ -0,0 +1,48 @@
+//Leetcode Q.232. Implement Queue using Stacks
+
+//only logic part
+
+class MyQueue {
+public:
+    stack<int> stk;
+    
+    MyQueue() {
+        
+    }
+    
+     void push(int x) {
+        stack<int> tmp;
+        while(!stk.empty()){
+            tmp.push(stk.top());
+            stk.pop();
+        }
+        stk.push(x);
+        while(!tmp.empty()){
+            stk.push(tmp.top());
+            tmp.pop();
+        }
+    }
+    
+    int pop() {
+        int tmp = stk.top();
+        stk.pop();
+        return tmp;
+    }
+   
+    int peek() {
+        return stk.top();
+    }
+
+    bool empty() {
+        return stk.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
