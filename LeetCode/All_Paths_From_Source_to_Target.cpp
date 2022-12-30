23411bf2163b2f2fa0f97f444a998db8ac916262
UKnow-IKnow <sarojsahu014@gmail.com>
2022-12-30 22:28:49 +0530

    Solution of All Paths From Source to Target Leetcode(Q.797)

---
+++ LeetCode/All_Paths_From_Source_to_Target.cpp
@@ -0,0 +1,29 @@
+//Leetcode Q.797. All Paths From Source to Target
+
+//only logic part
+
+class Solution {
+public:
+    void dfs(vector<vector<int>>& graph, int cur, vector<int>& path, vector<vector<int>>& paths){
+        int n = graph.size();
+        
+        if(cur == n-1){
+            paths.push_back(path);
+        }else{
+            for(int nei : graph[cur]){
+                path.push_back(nei);
+                dfs(graph, nei, path, paths);
+                path.pop_back();
+            }
+        }
+    };
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> path = {0};
+        vector<vector<int>> paths;
+        
+        dfs(graph, 0, path, paths);
+        
+        return paths;
+    }
+};
