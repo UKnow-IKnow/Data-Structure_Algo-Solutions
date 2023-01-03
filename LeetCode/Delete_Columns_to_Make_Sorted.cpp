9e63b3207baf86015239b218e0efdc4e86cf14bb
UKnow-IKnow <sarojsahu014@gmail.com>
2023-01-03 22:17:55 +0530

    Solution of Delete Columns to Make Sorted Leetcode(Q.944)

---
+++ LeetCode/Delete_Columns_to_Make_Sorted.cpp
@@ -0,0 +1,20 @@
+//Leetcode Q.944. Delete Columns to Make Sorted
+
+//only logic part
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size(), m = strs[0].size();
+
+        int ans = 0;
+        for (int j = 0; j < m; j++)
+            for (int i = 1; i < n; i++)
+                if (strs[i][j] < strs[i - 1][j]) {
+                    ans++;
+                    break;
+                }
+
+        return ans;
+    }
+};
