dc45ca4fc19ebf16832bb87d5851e6e44f46127a
UKnow-IKnow <sarojsahu014@gmail.com>
2023-01-26 22:39:50 +0530

    Solution of Cheapest Flights Within K Stops Leetcode(Q.787)

---
+++ LeetCode/Cheapest_Flights_Within_K_Stops.cpp
@@ -0,0 +1,30 @@
+//Leetcode Q.787. Cheapest Flights Within K Stops
+
+//only logic part
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        int MAX = 1e6+1;
+        vector<int> dist(n, MAX);
+        dist[src] = 0;
+        for(vector<int>& flight : flights){
+            
+            if(flight[0] == src)
+                dist[flight[1]] = flight[2];
+        }
+        
+       
+        while(K-- > 0){
+            
+            vector<int> tmp(dist);
+            for(vector<int>& flight : flights){
+                
+                tmp[flight[1]] = min(tmp[flight[1]], dist[flight[0]] + flight[2]);
+            }
+            swap(tmp, dist);
+        }
+        
+        return dist[dst] == MAX ? -1 : dist[dst];
+    }
+};
