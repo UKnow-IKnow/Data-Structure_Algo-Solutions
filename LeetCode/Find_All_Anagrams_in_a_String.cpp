99e3a4996b47963f3ce9c8c83255b72d10ae83a4
UKnow-IKnow <sarojsahu014@gmail.com>
2023-02-05 21:58:03 +0530

    Solution of Find All Anagrams in a String Leetcode(Q.438)

---
+++ LeetCode/Find_All_Anagrams_in_a_String.cpp
@@ -0,0 +1,51 @@
+//Leetcode Q.438. Find All Anagrams in a String
+
+// only logic part
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        map<char, int> pcount;
+        for(char c: p){
+            if(pcount.find(c) == pcount.end()){
+                pcount[c] = 1;
+            }else{
+                pcount[c]++;
+            }
+        }
+        
+        map<char, int> wcount; 
+        int start = 0, end = 0, match = 0;
+        
+        while(end < s.size()){
+            char c1 = s[end];
+            if(pcount.find(c1) != pcount.end()){
+                
+                if(wcount.find(c1) == wcount.end()){
+                    wcount[c1] = 1;
+                }else{
+                    wcount[c1]++;
+                }
+                
+                if(pcount[c1] == wcount[c1])match++;
+            }
+            
+            
+            while(match == pcount.size()){
+                if(end - start + 1 == p.size()){
+                    ans.push_back(start);
+                }
+                
+                char c2 = s[start];
+                if(pcount.find(c2) != pcount.end()){
+                    wcount[c2]--;
+                    if(wcount[c2] < pcount[c2])match--;
+                }
+                start++;
+            }
+            end++;
+        }
+        return ans;
+    }
+};
