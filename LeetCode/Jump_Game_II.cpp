372f3c64a1ffadcc0914faa0a434f4d93a68d359
UKnow-IKnow <sarojsahu014@gmail.com>
2023-02-08 20:59:31 +0530

    Solution of Jump Game II Leetcode(Q.45)

---
+++ LeetCode/Jump_Game_II.cpp
@@ -0,0 +1,20 @@
+//Leetcode Q.45. Jump Game II
+
+//only logic part
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, INT_MAX);
+        
+        dp[0] = 0;
+        
+        for(int i = 0; i < n; ++i){
+            for(int j = i+1; j <= min(n-1, i+nums[i]); ++j){
+                dp[j] = min(dp[j], dp[i]+1);
+            }
+        }
+        return dp[n-1];
+    }
+};
