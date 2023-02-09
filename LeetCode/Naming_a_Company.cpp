fffaa00b2abbadd46a1eb7df3cc10bc31d4bb647
UKnow-IKnow <sarojsahu014@gmail.com>
2023-02-09 22:47:13 +0530

    Solution of Naming a Company Leetcode(Q.2306)

---
+++ LeetCode/Naming_a_Company.cpp
@@ -0,0 +1,30 @@
+//Leetcode Q.2306. Naming a Company
+
+//only logic part
+
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        unordered_set<string> seen(ideas.begin(), ideas.end()); 
+        unordered_set<char> first; 
+        vector<vector<int>> freq(26, vector<int>(26)); 
+        for (auto& idea : ideas) 
+            first.insert(idea[0]); 
+        for (auto idea : ideas) {
+            char c = idea[0]; 
+            for (auto& cc : first) {
+                idea[0] = cc; 
+                if (!seen.count(idea)) ++freq[c-'a'][cc-'a']; 
+            }
+        }
+        long long ans = 0; 
+        for (auto idea : ideas) {
+            char c = idea[0]; 
+            for (auto& cc : first) {
+                idea[0] = cc; 
+                if (!seen.count(idea)) ans += freq[cc-'a'][c-'a']; 
+            }
+        }
+        return ans; 
+    }
+};
