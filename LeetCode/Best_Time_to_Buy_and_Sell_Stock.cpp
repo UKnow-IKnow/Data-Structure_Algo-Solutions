4163f42f021cb94e593f2fa6c137f017edda9952
UKnow-IKnow <sarojsahu014@gmail.com>
2023-02-25 23:43:53 +0530

    Solution of  Best Time to Buy and Sell Stock Leetcode(Q.121)

---
+++ LeetCode/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,26 @@
+//Leetcode Q.121. Best Time to Buy and Sell Stock
+
+//only logic part
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0) return 0;
+        
+        int ans = 0;
+        
+        int start = prices[0], end = prices[0];
+        
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < start){
+                ans = max(ans, end - start);
+                start = prices[i];
+                end = prices[i];
+            }else{
+                end = max(end, prices[i]);
+            }
+        }
+        ans = max(ans, end - start);
+        return ans;
+    }
+};
