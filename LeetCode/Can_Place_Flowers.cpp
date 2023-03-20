425d8ea6444c87cdb2bc6370a7a71fa4df713244
UKnow-IKnow <sarojsahu014@gmail.com>
2023-03-20 20:24:58 +0530

    Solution of Can Place Flowers Leetcode(Q.605)

---
+++ LeetCode/Can_Place_Flowers.cpp
@@ -0,0 +1,23 @@
+//Leetcode Q.605. Can Place Flowers
+
+//only logic part
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int N = flowerbed.size();
+        int count = 0;
+        int i = 0;
+        while (i < N){
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == N - 1 || flowerbed[i + 1] == 0)){
+                flowerbed[i++] = 1;
+                count++;
+            }
+            if (count >= n)
+                return true;
+
+            i++;
+        }
+        return false;
+    }
+};
