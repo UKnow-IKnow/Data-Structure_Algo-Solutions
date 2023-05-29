092875b25514ca5f5b00b22a9966d4ccca484fc1
UKnow-IKnow <sarojsahu014@gmail.com>
2023-05-29 22:28:11 +0530

    Solution of Design Parking System Leetcode(Q.1603)

---
+++ LeetCode/Design_Parking_System.cpp
@@ -0,0 +1,26 @@
+//1603. Design Parking System
+
+//only logic part
+
+class ParkingSystem {
+public:
+    vector<int> counts;
+    
+    ParkingSystem(int big, int medium, int small) {
+        counts = {big, medium, small};
+    }
+    
+    bool addCar(int carType) {
+        if(counts[carType-1] > 0){
+            --counts[carType-1];
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
