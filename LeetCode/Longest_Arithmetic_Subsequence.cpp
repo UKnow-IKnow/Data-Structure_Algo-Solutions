8fbd60b7468b7ba2fda5c56616d1bdf60b225cc0
UKnow-IKnow <sarojsahu014@gmail.com>
2023-06-23 22:06:57 +0530

    Solution of Longest Arithmetic Subsequence Leetcode(Q.1027)

---
+++ LeetCode/Longest_Arithmetic_Subsequence.cpp
@@ -0,0 +1,34 @@
+//Leetcode Q.1027. Longest Arithmetic Subsequence
+
+//only logic part
+
+class Solution {
+public:
+
+    int longestArithSeqLength(vector<int>& nums) {
+        
+        int n = nums.size();
+        int ans = 0;
+
+            if(n <= 2)
+             return n;
+             
+         unordered_map<int,int> dp[n+1];
+
+         for(int i=1;i<n;i++){
+             for(int j=0;j<i;j++){
+
+                 int diff = nums[i]-nums[j];
+                 int cnt = 1;
+
+                 if(dp[j].count(diff))
+                  cnt = dp[j][diff];
+
+                  dp[i][diff] = 1 + cnt;  
+                  ans = max(ans,dp[i][diff]);
+             }
+         }
+
+        return ans;
+    }
+};
