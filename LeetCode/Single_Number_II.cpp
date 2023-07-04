b981a7f56942bfc72df441f66217328392d12a93
UKnow-IKnow <sarojsahu014@gmail.com>
2023-07-04 22:07:26 +0530

    Solution of Single Number II Leetcode(Q.137)

---
+++ LeetCode/Single_Number_II.cpp
@@ -0,0 +1,27 @@
+//Leetcode Q.137. Single Number II
+
+//only logic part
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        
+        for(int i = 31; i >= 0; i--){
+            int mask = 1 << i;
+            int set = 0;
+            
+            for(int num : nums){
+                if(num & mask){
+                    set++;
+                }
+            }
+            
+            if(set%3 == 1){
+                ans += mask;
+            }
+        }
+        
+        return ans;
+    }
+};
