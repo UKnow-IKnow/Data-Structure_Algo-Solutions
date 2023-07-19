090526c1779749c72ad4e37429bf6a1b6a18f3fb
UKnow-IKnow <sarojsahu014@gmail.com>
2023-07-19 21:55:52 +0530

    Solution of Non-overlapping Intervals Leetcode(Q.435)

---
+++ LeetCode/Non-overlapping_Intervals.cpp
@@ -0,0 +1,27 @@
+//Leetcode Q.435. Non-overlapping Intervals
+
+//only logic part
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        
+        sort(intervals.begin(), intervals.end(),
+            [](vector<int>& a, vector<int>& b){
+                return a[1] < b[1];
+            });
+        
+        int discarded = 0;
+        int last_end = INT_MIN;
+        
+        for(vector<int> interval : intervals){
+            if(last_end > interval[0])
+                ++discarded;
+            else
+                last_end = interval[1];
+        }
+        
+        return discarded;
+    }
+};
