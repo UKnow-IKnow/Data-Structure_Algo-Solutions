688c3a63b1756264bb8ca91e3bdcb1ab91d2935c
UKnow-IKnow <sarojsahu014@gmail.com>
2023-08-03 00:07:05 +0530

    Solution of Permutations Leetcode(Q.46)

---
+++ LeetCode/Permutations.cpp
@@ -0,0 +1,30 @@
+//Leetcode Q.46. Permutations
+
+//only logic part
+
+class Solution {
+public:
+    void backtrack(vector<vector<int>>& pers, vector<int>& per, vector<int>& nums){
+        if(per.size() == nums.size()){
+            pers.push_back(per);
+        }else{
+            for(int e : nums){
+                if(find(per.begin(), per.end(), e) != per.end()){
+                    continue;
+                }
+                per.push_back(e);
+                backtrack(pers, per, nums);
+                per.pop_back();
+            }
+        }
+    };
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> pers;
+        vector<int> per;
+        
+        backtrack(pers, per, nums);
+        
+        return pers;
+    }
+};
